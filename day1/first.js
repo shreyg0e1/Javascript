65838880cbd9b168f57111acf4d7e4d3e1c9bdfa
Shrey <shreyg0e1dev@gmail.com>
2026-02-24 23:49:43 +0530

    Day 1: JavaScript practice

---
+++ day1/first.js
@@ -0,0 +1,20 @@
+console.log("Radhe Radhe");
+
+//press { ctrl + `} together to open a terminal //
+// to declare their aare 3 methods:- let const var
+// var is outdated will not use it
+
+let num=30;
+num="shrey"
+console.log(num);
+
+// const id="shrey";
+// //  this will showing error 
+// id=10;
+// console.log(id);
+
+
+//just to show but will avoid using it
+var n=20;
+n= 45;
+console.log(n);
