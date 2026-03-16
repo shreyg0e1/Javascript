9db6a4123a77b2bb5e4828d07195e18ded124c21
Shrey <shreyg0e1dev@gmail.com>
2026-03-16 10:22:19 +0530

    Day 19: Javascript practice

---
+++ day19/first.js
@@ -0,0 +1,9 @@
+// class Person{
+//     constructor(){
+
+//     }
+// }
+
+// to avoid the conflict because class keyword is reserved for creating class therefore, className is used
+// const obj= document.getElementById("first");
+// obj.className="bhosdo";
